8d021be1ea197af15e855a879a68b8df18578ed1
Mopewa Ogundipe <mopewa.o@gmail.com>
2014-04-22 17:43:25 -0400

    added schedule page

--- homepage/views.py
+++ homepage/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def index(request):
-    # request context of the request (eg: client's machine details)
+    return render_to_response('homepage/index.html');
+
+def schedule(request):
     context = RequestContext(request)
 
-    # dict to pass to template engine as its context
     context_dict = {}
 
-    # return rendered response to send to client
-    return render_to_response('homepage/index.html', context_dict, context);
+    return render_to_response('homepage/schedule.html', context_dict, context);
--- schedule_fillr/urls.py
+++ schedule_fillr/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('homepage.urls')),
+    url(r'^', include('homepage.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
