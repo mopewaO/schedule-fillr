825985e737e529a466e8001ee6a543ea8d058d46
Pretzelish <Pretzelish@github.com>
2014-04-25 16:36:35 -0400

    ICS parser

---
+++ schedule_fillr/icsparser.py
@@ -0,0 +1,13 @@
+import tempfile
+import re
+
+#pass a file, get list of courses w/ lecture/recitation number
+def parse_ics_file(f):
+	filetxt = f.read()
+	courselist = re.split(r"BEGIN:VEVENT", filetxt)
+	callist = []
+	for course in courselist:
+		coursename = re.search(r"SUMMARY:[^:]+:: (\d\d\d\d\d) ([A-Z\d])", course)
+		if coursename != None:
+			callist.append(coursename.group(1) + " " + coursename.group(2))
+        return callist
